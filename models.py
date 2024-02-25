3beffd1b3739f1ebd2c50d914c35153b58fa696c
OniMirage01 <audavidson1@yahoo.com>
2024-02-24 23:14:37 -0800

    #YPS-80, #YPS-81 #dynamically interact with fake data. This will allow me to test and demonstrate various functionalities without relying on real-world data during development. Implemented in both Client Track Page and Provider Track Page.

--- models.py
+++ models.py
@@ -8,17 +8,6 @@
 from django.db import models
 
 
-class Admin(models.Model):
-    admin_id = models.AutoField(primary_key=True)
-    admin_username = models.CharField(unique=True, max_length=50, blank=True, null=True)
-    admin_password = models.CharField(max_length=255, blank=True, null=True)
-    admin_email = models.CharField(max_length=100, blank=True, null=True)
-    admin_phone = models.CharField(max_length=15, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'ADMIN'
-
 
 class Appointments(models.Model):
     appointment_id = models.AutoField(primary_key=True)
@@ -26,20 +15,18 @@ class Appointments(models.Model):
     emp = models.ForeignKey('Employee', models.DO_NOTHING, blank=True, null=True)
     appdate = models.DateField(blank=True, null=True)
     apptime = models.TimeField(blank=True, null=True)
-    admin_id = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'APPOINTMENTS'
-
 
 class Client(models.Model):
     client_id = models.AutoField(db_column='Client_id', primary_key=True)  # Field name made lowercase.
     fname = models.CharField(max_length=100, blank=True, null=True)
     lname = models.CharField(max_length=100, blank=True, null=True)
     email = models.CharField(unique=True, max_length=100, blank=True, null=True)
-    cl_password = models.CharField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    cl_password = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
@@ -48,13 +35,12 @@ class Client(models.Model):
 
 
 class Company(models.Model):
-    c_id = models.AutoField(db_column='C_id', primary_key=True)  # Field name made lowercase.
-    company_name = models.CharField(db_column='Company_Name', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    company_address = models.CharField(db_column='Company_Address', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    company_phone = models.CharField(db_column='Company_Phone', max_length=15, blank=True, null=True)  # Field name made lowercase.
-    company_email = models.CharField(db_column='Company_Email', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    company_pw = models.CharField(db_column='Company_PW', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    admin = models.ForeignKey(Admin, models.DO_NOTHING, blank=True, null=True)
+    C_id = models.AutoField(primary_key=True, db_column='C_id')
+    company_name = models.CharField(max_length=100, db_column='Company_Name', blank=True, null=True)
+    company_address = models.CharField(max_length=255, db_column='Company_Address', blank=True, null=True)
+    company_phone = models.CharField(max_length=15, db_column='Company_Phone', blank=True, null=True)
+    company_email = models.CharField(max_length=100, db_column='Company_Email', blank=True, null=True)
+    company_pw = models.CharField(max_length=255, db_column='Company_PW', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -62,21 +48,20 @@ class Company(models.Model):
 
 
 class Employee(models.Model):
-    employee_id = models.AutoField(db_column='Employee_id', primary_key=True)  # Field name made lowercase.
+    employee_id = models.AutoField(primary_key=True, db_column='Employee_id')
     fname = models.CharField(max_length=50, blank=True, null=True)
     lname = models.CharField(max_length=50, blank=True, null=True)
     c = models.ForeignKey(Company, models.DO_NOTHING, blank=True, null=True)
-    emp_password = models.CharField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    is_admin = models.IntegerField(blank=True, null=True)
-    admin = models.ForeignKey(Admin, models.DO_NOTHING, blank=True, null=True)
+    emp_password = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    is_admin = models.CharField(max_length=1, choices=[('Y', 'Yes'), ('N', 'No')], default='N')
 
     class Meta:
         managed = False
         db_table = 'EMPLOYEE'
 
 
-class Payment(models.Model):
+class Invoice(models.Model):
     payment_id = models.AutoField(primary_key=True)
     client = models.ForeignKey(Client, models.DO_NOTHING, blank=True, null=True)
     emp = models.ForeignKey(Employee, models.DO_NOTHING, blank=True, null=True)
@@ -89,12 +74,10 @@ class Payment(models.Model):
     email = models.CharField(max_length=100, blank=True, null=True)
     reference_number = models.CharField(max_length=50, blank=True, null=True)
     status = models.CharField(max_length=20, blank=True, null=True)
-    admin_id = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'PAYMENT'
-
 
 class Taskping(models.Model):
     task_id = models.AutoField(primary_key=True)
@@ -103,7 +86,6 @@ class Taskping(models.Model):
     description = models.TextField(blank=True, null=True)
     emp = models.ForeignKey(Employee, models.DO_NOTHING, blank=True, null=True)
     client = models.ForeignKey(Client, models.DO_NOTHING, blank=True, null=True)
-    admin_id = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
